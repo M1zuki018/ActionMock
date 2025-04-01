a3023c9c7884b6e2090625465bb56c2f6ce30a94
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-04-01 22:23:21 +0900

    放物線を描くジャンプ挙動を作成

--- Assets/Scripts/Dev/Jump.cs
+++ Assets/Scripts/Dev/Jump.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 /// <summary>
@@ -8,17 +9,30 @@ public class Jump : MonoBehaviour
 {
     [SerializeField] private Transform _player;
     [SerializeField] private List<GameObject> _props;
-    private int _index;
+    [SerializeField] private Ease _jumpEase;
+    private int _index = 0;
+    private float _playerHeight = 2f;
     
     [MethodButtonInspector]
     public void Test()
     {
         if (_index < _props.Count)
         {
+            /*
             // プレイヤーの座標を次のプロップのポジションに移動させる
             Vector3 vector = _props[_index].transform.position;
             vector.y += 2; // プレイヤーの高さを足している
             _player.position = vector;
+            */
+            
+            Vector3 endPos = _props[_index].transform.position;
+            endPos.y += _playerHeight;
+            
+            ParabolicMove(
+                start: _player.position, 
+                end: endPos,
+                duration: 1f,
+                height: 0.8f + _playerHeight);
         }
         _index++;
     }
@@ -29,5 +43,22 @@ public class Jump : MonoBehaviour
         {
             Test(); // キーボードからの発火
         }
+    }
+
+    /// <summary>
+    /// 放物戦を描くようなジャンプ軌道
+    /// </summary>
+    private void ParabolicMove(Vector3 start, Vector3 end, float duration, float height)
+    {
+        _player.position = start;
+        
+        // 中間点を計算
+        Vector3 midPoint = (start + end) / 2f;
+        midPoint.y += height;
+        
+        // 経路
+        Vector3[] path = {midPoint, end};
+        
+        _player.DOPath(path, duration, PathType.CatmullRom).SetEase(_jumpEase);
     }
 }
