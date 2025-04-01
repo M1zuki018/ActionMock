7df8f07392420d1a1a9f6685d77de3b4671b6561
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-04-01 20:18:43 +0900

    ボタンを押して移動

--- Assets/Scripts/Dev/Jump.cs
+++ Assets/Scripts/Dev/Jump.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 /// <summary>
-/// 
+/// タイミングに合わせてジャンプする部分のテスト
 /// </summary>
-public class Jump
+public class Jump : MonoBehaviour
 {
+    [SerializeField] private Transform _player;
+    [SerializeField] private List<GameObject> _props;
+    private int _index;
     
+    [MethodButtonInspector]
+    public void Test()
+    {
+        if (_index < _props.Count)
+        {
+            // プレイヤーの座標を次のプロップのポジションに移動させる
+            Vector3 vector = _props[_index].transform.position;
+            vector.y += 2; // プレイヤーの高さを足している
+            _player.position = vector;
+        }
+        _index++;
+    }
 }
