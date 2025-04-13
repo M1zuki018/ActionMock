56bb5cb9f2c3a4a3b8568c90cb9a2262477db913
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-04-13 11:41:26 +0900

    ロックオンとアイコンの処理を作成

---
+++ Assets/Scripts/Dev/Mock2/LockOn.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーが敵をロックオンして弾を撃つシステム
+/// </summary>
+public class LockOn : MonoBehaviour
+{
+    [SerializeField] private Transform _lockOnIcon;
+    [SerializeField] private GameObject _bulletPrefab;
+    private List<Transform> _allTargets = new List<Transform>();
+    private Transform _currentTarget; // 現在のターゲット
+    private int _targetIndex = 0;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+        
+        // すべての Target タグを持つオブジェクトを取得し、距離でソートして _allTargets に格納
+        Vector3 playerPosition = transform.position;
+    
+        _allTargets = GameObject.FindGameObjectsWithTag("Target")
+            .Select(go => go.transform)
+            .OrderBy(t => Vector3.Distance(t.position, playerPosition))
+            .ToList();
+        
+        _currentTarget = _allTargets[_targetIndex];
+    }
+
+    private void Update()
+    {
+        Vector3 screenPosition = _camera.WorldToScreenPoint(_currentTarget.position);
+        _lockOnIcon.transform.position = screenPosition; // ターゲットアイコンを現在のターゲットに合わせる
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            TargetBroken();
+        }
+    }
+
+    /// <summary>
+    /// ターゲット破壊
+    /// </summary>
+    private void TargetBroken()
+    {
+        SearchTarget();
+    }
+    
+    /// <summary>
+    /// 次のターゲットを探す
+    /// </summary>
+    private void SearchTarget()
+    {
+        _targetIndex++;
+        if (_targetIndex < _allTargets.Count)
+        {
+            _currentTarget = _allTargets[_targetIndex];
+        }
+        else
+        {
+            _lockOnIcon.gameObject.SetActive(false); // ターゲットがいなくなったらアイコンを非表示にする
+        }
+        
+    }
+}
